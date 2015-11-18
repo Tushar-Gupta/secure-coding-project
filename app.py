8a761b1df48f086a18ddf5badb62bf24b6da26b4
rohan katyal <rohan12086@iiitd.ac.in>
2015-11-18 19:40:19 +0530

    edit user profile sql procedure

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'abcd'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'StudentPortal'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
